c264f37be904802e4fb4de4c806c694e439d79c2
ygonzalez <ygonzalez704@gmail.com>
2024-08-02 12:51:43 -0500

    sample apps

---
+++ sample_project1/.ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import requests
+from datetime import datetime, timedelta
+import boto3
+
+st.set_page_config(page_title="TEST STREAMLIT APP 1")
+
+st.title("TEST STREAMLIT APP 1")
---
+++ sample_project2/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import requests
+from datetime import datetime, timedelta
+import boto3
+
+st.set_page_config(page_title="TEST STREAMLIT APP 2")
+
+st.title("TEST STREAMLIT APP 2")
